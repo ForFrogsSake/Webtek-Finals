f699342076101fb1d93564976437ef505971ec61
blehli <2167338@slu.edu.ph>
2018-05-11 20:37:47 +0800

    nasa loob na ng web folder ng truckrentalsclient project ung sa bootstrap

--- Java - Client/truckRentalsClient/src/java/dbase/connection/DBConnect.java
+++ Java - Client/truckRentalsClient/src/java/dbase/connection/DBConnect.java
@@ -6,6 +6,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class DBConnect {
     private Connection connection;
@@ -37,5 +40,20 @@ public class DBConnect {
         } catch (SQLException e) {
             System.err.println("error: " + e.getClass() + "\n" + e.getMessage());
         }
+    }
+  /**  
+    public void createUser(String email, String fname, String lname, String mname, String password, String username){
+        Calendar date = Calendar.getInstance(getDate());
+	System.out.println(dateFormat.format(cal));
+        query = "insert into user values(?);";
+        preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1, dname);
+        preparedStatement.executeUpdate();
+    }
+*/    
+    public ResultSet getTrucks() throws SQLException {
+        statement= connection.createStatement();
+        query="select * from trucks";
+        return statement.executeQuery(query);
     }
 }
--- Java - Client/truckRentalsClient/src/java/dbase/connection/DBConnectStartEnd.java
+++ Java - Client/truckRentalsClient/src/java/dbase/connection/DBConnectStartEnd.java
@@ -6,16 +6,21 @@ public class DBConnectStartEnd {
     private static DBConnect connector;
     
     public static void main(String[]args){
+        String start = "yes"; // remember to refer sa login part
         connector = new DBConnect();
         try {
-            String url= "jdbc:mysql://localhost/truck_rental?useSSL=false";
-            connector.dbaseConnect(url, "root", "");
+            if (start.equals("yes")){
+                String url= "jdbc:mysql://localhost/truck_rentals?useSSL=false";
+                connector.dbaseConnect(url, "root", "");
+            } else {
+                connector.close();
+            }
         } catch (SQLException e) {
             System.err.println("error: " + e.getClass() + "\n" + e.getMessage());
         } catch (Exception e) {
             System.err.println("error: " + e.getClass() + "\n" + e.getMessage());
         } finally{
-            
+            connector.close();
         }
         
     }
